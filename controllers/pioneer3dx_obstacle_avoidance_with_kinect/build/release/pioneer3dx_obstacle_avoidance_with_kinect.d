004fab4d55c41996dd3b43fba6cfe5a2318a0652
tadun <56317154+tadun@users.noreply.github.com>
2025-11-07 13:05:57 +0000

    Controllers for two robots
    Separate controllers for the teacher and the student.

---
+++ controllers/pioneer3dx_obstacle_avoidance_with_kinect/build/release/pioneer3dx_obstacle_avoidance_with_kinect.d
@@ -0,0 +1,9 @@
+build/release/pioneer3dx_obstacle_avoidance_with_kinect.o: \
+  pioneer3dx_obstacle_avoidance_with_kinect.c \
+  /Applications/Webots.app/Contents/include/controller/c/webots/camera.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/camera_recognition_object.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/types.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/motor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/range_finder.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/nodes.h
---
+++ controllers/pioneer3dx_teacher/build/release/pioneer3dx_teacher.d
@@ -0,0 +1,8 @@
+build/release/pioneer3dx_teacher.o: pioneer3dx_teacher.c \
+  /Applications/Webots.app/Contents/include/controller/c/webots/camera.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/camera_recognition_object.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/types.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/motor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/range_finder.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/nodes.h
